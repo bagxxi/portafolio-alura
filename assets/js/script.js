ba5ea963f3774f9368dd243cb3136af9a7662730
bagxxi <bagxxi@gmail.com>
2022-05-13 10:52:41 -0400

    initial commit
    Added completed site

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+import { validar, habilitarBotonEnviar } from "./validaciones.js";
+
+const inputs = document.querySelectorAll(".input");
+const enviar = document.querySelector("[data-submit]");
+
+inputs.forEach((input) => {
+
+    input.addEventListener("blur", (input) => {
+        validar(input.target)
+    });
+
+    input.addEventListener("keyup", (input) => {    // comprueba despues de ingresar un caracter del teclado
+
+        // if ( tipoDeInput == "nombre"){        // comprueba solo el campo data-tipo="nombre" en cada ingreso de teclado
+        //       validar(input.target);
+        // }   
+
+        validar(input.target);
+        habilitarBotonEnviar(inputs);
+    });
+});
+
+enviar.addEventListener("click", () => {
+
+    // inputs.forEach((input) => {
+    //     validar(input);
+    // });
+
+});
